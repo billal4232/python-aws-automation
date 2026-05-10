8317d90fe07ce4a19722a774e445a966e2859602
billal4232 <billal4232@gmail.com>
2026-05-10 20:00:46 +0300

    Update terminate_ec2.py

--- terminate_ec2.py
+++ terminate_ec2.py
@@ -1,44 +1,31 @@
 import boto3
+session = boto3.Session(profile_name = "limonlab", region_name = "eu-north-1")
 
-# Connect to AWS using limonlab profile and eu-north-1 region
-session = boto3.Session(profile_name='limonlab', region_name='eu-north-1')
+ec2 = session.client("ec2")
 
-# Create EC2 client to interact with EC2 service
-ec2 = session.client('ec2')
+response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
-# Get all EC2 instances from AWS
-response = ec2.describe_instances()
 
-# Ask user which instance they want to terminate
-search_name = input("Type instance name to terminate: ")
+instance_ids = []
 
-# Assume no match found yet
-found = False
-
-# Loop through AWS response structure to reach each instance
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
-
-        # Get Name tag safely — returns empty list if no tags exist
-        name = ''
-        for tag in instance.get('Tags', []):
+        name = ""
+        for tag in instance.get('Tags',[]):
             if tag['Key'] == 'Name':
                 name = tag['Value']
+        print(f"Instance Id: {instance['InstanceId']}, Instance Type: {instance['InstanceType']}, State: {instance['State']['Name']}, Name: {name} ")
+        instance_ids.append(instance['InstanceId'])
 
-        # Check if this instance matches the name user typed
-        if name == search_name:
-            print(f"Found: {instance['InstanceId']} — {name}")
+instance_id = input("Enter the Instance Id you want to terminate: ")
 
-            # Ask for confirmation before terminating
-            confirm = input("Are you sure you want to terminate? (yes/no): ")
-            if confirm == "yes":
-                ec2.terminate_instances(InstanceIds=[instance['InstanceId']])
-                print("Terminated.")
-                found = True
-            else:
-                print("Cancelled.")
-                found = True
+if instance_id in instance_ids:
+    confirm = input(f"Are you sure you want to terminate {instance_id}? (yes/no): ")
 
-# If no instance matched the name typed
-if not found:
-    print("No instance found with that name.")
+    if confirm == "yes":
+       response = ec2.terminate_instances(InstanceIds=[instance_id])
+       print("Terminated")
+    else:
+        print("Cancelled")
+else:
+    print("No valid id is found")
